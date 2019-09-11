c1574b4b71d133f96409be1b02807443abaff361
Matheus de Campos <Matheus.campos@edu.unifil.br>
2019-09-11 01:36:49 -0300

    update

---
+++ Seção 3 Recapitulação de Lógica de Programação usando C#/Projetos/Exe_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_02
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            Console.WriteLine("Insira as informações conforme o fluxo");
+            Console.WriteLine("========================================");
+
+            Console.Write("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+            
+
+            Console.Write("Quantos quartos tem na sua casa: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o preço de um produto: ");
+            double valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Entre seu último nome, idade e altura(Na mesma linha)");
+            string[] vetor = Console.ReadLine().Split(' ');
+            string sobrenome = vetor[0];
+            int idade = int.Parse(vetor[1]);
+            double altura = double.Parse(vetor[2]);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Sua ficha");
+            Console.WriteLine(nome);
+            Console.WriteLine(quantidade);
+            Console.WriteLine(valor.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("=================");
+            Array.ForEach(vetor, Console.WriteLine); // Imprimi conteudo do vetor
+
+        }
+    }
+}
---
+++ Seção 3 Recapitulação de Lógica de Programação usando C#/Projetos/Exe_EntradaDados2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_EntradaDados2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            string nome = vetor[0];
+            char sexo = char.Parse(vetor[1]);
+            int idade = int.Parse(vetor[2]);
+            double altura = double.Parse(vetor[3],CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou :" + n1);
+            Console.WriteLine("Você digitou :" + ch);
+            Console.WriteLine("Você Digitou :" + n2.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
