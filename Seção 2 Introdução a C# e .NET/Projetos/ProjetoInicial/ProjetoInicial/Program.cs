221c4dac39365f8b0b66e072f300a3f27fc89123
Matheus de Campos <Matheus.campos@edu.unifil.br>
2019-09-11 00:01:52 -0300

    update

---
+++ Seção 2 Introdução a C# e .NET/Projetos/ProjetoInicial/ProjetoInicial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoInicial
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Primeiro Programa");
+        }
+    }
+}
---
+++ Seção 3 Recapitulação de Lógica de Programação usando C#/Projetos/Exe_EntradaDados/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exe_EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite três palavras: ");
+            string s = Console.ReadLine(); // lê o conteudo da linha e add dentro da variavel correspondente.
+            string[] vetor = s.Split(' '); // variavel correspondente chama o método Split que separada as variaveis entre os espaços em branco.
+            string palavra1 = vetor[0]; // add o conteudo nas posições especificas dentro do vetor.
+            string palavra2 = vetor[1];
+            string palavra3 = vetor[2];
+
+            Console.WriteLine("Você inseriu as seguintes palavra: " + s);
+        }
+    }
+}
+
