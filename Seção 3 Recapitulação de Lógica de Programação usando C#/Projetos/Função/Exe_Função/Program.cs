c5d90e85fe34a4f5baa0ead63ea0bddf4d7b617f
Matheus de Campos <matheus.campos@edu.unifil.br>
2019-09-11 10:25:39 -0300

    update

---
+++ Seção 3 Recapitulação de Lógica de Programação usando C#/Projetos/Função/Exe_Função/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Função
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine("Digite três valores: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+            Console.WriteLine("Maior: " + resultado);
+        }
+
+        static int Maior(int a, int b, int c) // Função
+        {
+            int m;
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+        }
+    }
+
+}
---
+++ Seção 4 Classes  atributos métodos membros estáticos/Projetos/Exe_Triangulo/Exe_Triangulo/Triangulo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exe_Triangulo
+{
+    class Triangulo
+    {
+        public double A, B, C;
+    }
+}
