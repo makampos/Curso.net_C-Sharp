fab0d0392e26e7bbdca032a96a9b5abea56e7c21
Matheus de Campos <Matheus.campos@edu.unifil.br>
2019-09-15 12:25:10 -0300

    update

---
+++ Seção 4 Classes  atributos métodos membros estáticos/Projetos/Projetos/Exe_membros_estáticos/ConversorMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exe_membros_estáticos
+{
+    class ConversorMoeda
+    {
+        public static double Iof = 0.6;    
+        public static double ValorPagoConvertido(double ValorCompra, double Cotacao)
+        {
+            double ValorIof = (ValorCompra * Iof) / 10 * Cotacao;
+            return  (ValorCompra * Cotacao + ValorIof)  ;
+        }      
+    }
+}
---
+++ Seção 4 Classes  atributos métodos membros estáticos/Projetos/Projetos/Exe_membros_estáticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_membros_estáticos
+{
+    class Program
+    {    
+            static void Main(String [] args)
+        {          
+            Console.Write("Qual é a cotação do dólar? ");
+            double Cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double ValorCompra = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Valor a ser pago em reais: ");
+
+            double ValorPagarReal = ConversorMoeda.ValorPagoConvertido(ValorCompra, Cotacao);
+
+            Console.WriteLine(ValorPagarReal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+    }
+}
