6074c4c600065225244e41a31a206b39221d9a5d
Matheus de Campos <Matheus.campos@edu.unifil.br>
2019-09-15 10:23:26 -0300

    update

---
+++ Seção 4 Classes  atributos métodos membros estáticos/Projetos/Exe_prop_03/Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exe_prop_03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double NotaA, NotaB, NotaC, NotaFinal, Media;
+
+
+        
+        public double CalcularNotaFinal()
+        {
+            NotaFinal = NotaA + NotaB + NotaC;
+            return NotaFinal;
+        }
+
+        public bool Aprovado()
+        {
+            if (CalcularNotaFinal() >= 60.0)
+            {
+                return true;
+                    
+            } else
+            return false;
+        }
+
+        public double Restante()
+        {
+            if(Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            return 60.0 - CalcularNotaFinal();
+        }
+       
+    }
+}
---
+++ Seção 4 Classes  atributos métodos membros estáticos/Projetos/Exe_prop_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exe_prop_03
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            // Executa o programa!
+
+            Aluno aln = new Aluno();
+
+            Console.WriteLine("Entre os dados do aluno:");
+
+            Console.Write("Nome do aluno: ");
+            aln.Nome = Console.ReadLine();
+
+            Console.WriteLine("Entre com as três notas do aluno: ");
+            aln.NotaA = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aln.NotaB = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aln.NotaC = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("NOTA FINAL: " + aln.CalcularNotaFinal().ToString(), ("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(" PONTOS");
+            if (aln.Aprovado())
+            {
+                Console.Write("=== APROVADO ===\n");                                        
+            }
+            else
+            {
+                Console.Write("=== REPROVADO ===\n");
+                Console.Write("FALTARAM: " + aln.Restante().ToString(), ("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine(" PONTOS");
+            }
+            
+        }
+    }
+}
